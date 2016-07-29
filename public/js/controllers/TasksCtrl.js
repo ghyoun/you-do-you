c0fe78d4b732d647544c2af1c81a9d045ca27355
Skyler Kidd <f.s.kidd@gmail.com>
2016-07-28 18:18:01 -0700

    Task stuff, pt1

--- public/js/controllers/TasksCtrl.js
+++ public/js/controllers/TasksCtrl.js
@@ -10,5 +10,25 @@
 
             $scope.select_sidebar_icon();
 
+            $scope.tasks = [];
+
+            $scope.new_task = function() {
+                var new_date = new Date();
+                var text_date = (new_date.getMonth()+1) + "/" + new_date.getDate() + "/" + new_date.getFullYear();
+                var task = {
+                    description : "",
+                    category : "",
+                    priority : "",
+                    due_date : text_date,
+                    status : "Not Started"
+                };
+                $scope.tasks.push(task);
+            };
+
+            $scope.delete_task = function(i) {
+                $scope.tasks.splice(i, 1);
+            };
+
+
         })
 })()
