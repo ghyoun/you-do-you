331a28bdd61f3ff37b57c0acd462ae9010693fdb
Skyler Kidd <f.s.kidd@gmail.com>
2016-07-29 03:37:53 -0700

    Dash load, please :)

--- public/js/controllers/DashCtrl.js
+++ public/js/controllers/DashCtrl.js
@@ -3,6 +3,12 @@
     angular
         .module('youDoYou')
         .controller('DashController', function($scope) {
+            
+            $scope.tasks = [];
+            $scope.goals = [];
+            $scope.finance_data = [];
+
+            
 
         })
 })()
